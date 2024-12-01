948cf7ebdd1b68fed6bf7f47e495c95148ec6ecf
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-12-01 11:36:21 +0200

    Adding mini surround

--- init.lua
+++ init.lua
@@ -186,6 +186,42 @@ require("lazy").setup({
   -- Automatic indentation
   "tpope/vim-sleuth",
 
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      require('mini.pairs').setup()
+    end,
+  },
    -- "gc" to comment visual regions/lines
   "numToStr/Comment.nvim",
 
@@ -419,9 +455,9 @@ require("lazy").setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-        zls = {
-          cmd = { "A:\\software\\zls\\zig-out\\bin\\zls.exe" },
-        },
+        -- zls = {
+        --   cmd = { "A:\\software\\zls\\zig-out\\bin\\zls.exe" },
+        -- },
 
         lua_ls = {
           -- cmd = {...},
