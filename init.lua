30016337ea5f214f4d011689e2200596302150f7
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-12-07 18:53:39 +0200

    Oil, lsp and telescope

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.showmode = false
 -- vim.opt.breakindent = true
--- vim.opt.undofile = true
+vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.signcolumn = "yes"
@@ -33,8 +33,15 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 
-
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kassu-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ timeout = 100 })
+  end,
+})
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>w", "\"+", { desc = "Clipboard registry" })
@@ -44,9 +51,6 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>");
 vim.keymap.set("n", "<leader>x", ":.lua<CR>");
 vim.keymap.set("v", "<leader>x", ":lua<CR>");
 
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
@@ -58,37 +62,7 @@ vim.keymap.set("n", "<up>", "<cmd>echo \"Use k to move!!\"<CR>")
 vim.keymap.set("n", "<down>", "<cmd>echo \"Use j to move!!\"<CR>")
 vim.keymap.set("i", "<C-v>", "<cmd>:normal \"+p <CR>")
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("kassu-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank({ timeout = 100 })
-  end,
-})
 
-
--- Remove bg
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "Visual", { bg = "#0398fc", fg = "#000000", })
-vim.api.nvim_set_hl(0, "cursorline", { bg = "#15191d" })
-vim.api.nvim_set_hl(0, "cursorlinenr", { bg = "#15191d", fg = "#e5ff00" })
--- print(
---   vim.api.nvim_get_hl(0, {name = "cursorline"}).sp,
---   vim.api.nvim_get_hl(0, {name = "cursorline"}).bg,
---   vim.api.nvim_get_hl(0, {name = "cursorline"}).fg,
---   vim.api.nvim_get_hl(0, {name = "cursorline"}).reverse
--- )
-
--- vim.api.nvim_create_autocmd({"ColorScheme", "VimEnter"}, {
---     group = vim.api.nvim_create_augroup('Color', {}),
---     pattern = "*",
---     callback = function ()
---         vim.api.nvim_set_hl(0, "LspReferenceRead", {bg = "#1d2022"})
---         vim.api.nvim_set_hl(0, "LspReferenceWrite", {bg = "#1d2022"})
---         vim.api.nvim_set_hl(0, "LspReferenceText", {bg = "#1d2022"})
---     end
--- })
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
@@ -110,4 +84,34 @@ if vim.g.neovide then
   vim.o.guifont = "FiraCode Nerd Font Mono"
   vim.keymap.set({"n", "x", "i"}, "<F11>", function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end)
 end
+
+
+-- Remove bg
+vim.api.nvim_set_hl(0, "Visual", { bg = "#0398fc", fg = "#000000", })
+vim.api.nvim_set_hl(0, "cursorline", { bg = "#15191d" })
+vim.api.nvim_set_hl(0, "cursorlinenr", { bg = "#15191d", fg = "#e5ff00" })
+
+-- Diagnostic keymaps
+-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- print(
+--   vim.api.nvim_get_hl(0, {name = "cursorline"}).sp,
+--   vim.api.nvim_get_hl(0, {name = "cursorline"}).bg,
+--   vim.api.nvim_get_hl(0, {name = "cursorline"}).fg,
+--   vim.api.nvim_get_hl(0, {name = "cursorline"}).reverse
+-- )
+
+-- vim.api.nvim_create_autocmd({"ColorScheme", "VimEnter"}, {
+--     group = vim.api.nvim_create_augroup('Color', {}),
+--     pattern = "*",
+--     callback = function ()
+--         vim.api.nvim_set_hl(0, "LspReferenceRead", {bg = "#1d2022"})
+--         vim.api.nvim_set_hl(0, "LspReferenceWrite", {bg = "#1d2022"})
+--         vim.api.nvim_set_hl(0, "LspReferenceText", {bg = "#1d2022"})
+--     end
+-- })
 
--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -8,20 +8,19 @@ return {
       install.compilers = { "zig", vim.fn.getenv('CC'), "cc", "gcc", "clang", "cl" }
 
       require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-	auto_install = false,
-	highlight = {
-	  enable = true,
-	  disable = { "c", "rust" },
-	  disable = function(lang, buf)
-	    local max_filesize = 100 * 1024 -- 100 KB
-	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	    if ok and stats and stats.size > max_filesize then
-	      return true
-	    end
-	  end,
-	  additional_vim_regex_highlighting = false,
-	},
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        auto_install = false,
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 101 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
       }
     end,
   }
