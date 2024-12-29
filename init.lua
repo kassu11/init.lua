aaceb818fbda31a4f0f22a3591c31f55c133f7e4
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-12-29 13:26:31 +0200

    Comment deprecated code

--- init.lua
+++ init.lua
@@ -52,8 +52,8 @@ vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = "Source current line" });
 vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Source visual selection" });
 vim.keymap.set("n", "<leader><leader>q", ":set nowrap!<CR>", { desc = "Toggle wrap" });
 
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic Error messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
+-- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic Error messages" })
+-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic Quickfix list" })
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
