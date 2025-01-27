6939afce577b2e64f111befed48cd2a2f72b89e8
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-01-27 14:03:32 +0200

    Project search recent files

--- lua/config/plugins/project.lua
+++ lua/config/plugins/project.lua
@@ -1,10 +1,26 @@
 return {
   {
-    'nvim-telescope/telescope-project.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
+    "nvim-telescope/telescope-project.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
-      require 'telescope'.load_extension('project')
+      local telescope = require "telescope"
+
       vim.keymap.set("n", "<leader>sp", function() vim.cmd("Telescope project") end)
+
+      local project_actions = require("telescope._extensions.project.actions")
+      local builtin = require "telescope.builtin"
+      telescope.setup {
+        extensions = {
+          project = {
+            on_project_selected = function(prompt_bufnr)
+              project_actions.change_working_directory(prompt_bufnr, false)
+              builtin.oldfiles { only_cwd = true }
+            end,
+          }
+        }
+      }
+
+      telescope.load_extension("project")
     end,
   }
 }
