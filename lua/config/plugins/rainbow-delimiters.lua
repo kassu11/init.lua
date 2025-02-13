d092ead1505e4cd07d81e4fd1cf8046454ff69ad
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-02-13 21:55:02 +0200

    Disable plugins

--- lua/config/plugins/rainbow-delimiters.lua
+++ lua/config/plugins/rainbow-delimiters.lua
@@ -1,3 +1,19 @@
 return {
-  { "HiPhish/rainbow-delimiters.nvim", }
+  {
+    enabled = false,
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      require 'rainbow-delimiters.setup'.setup {
+        highlight = {
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterBlue',
+        },
+      }
+
+      vim.cmd [[hi RainbowDelimiterYellow guifg=#ffd700 ctermfg=White]]
+      vim.cmd [[hi RainbowDelimiterViolet guifg=#da70d6 ctermfg=White]]
+      vim.cmd [[hi RainbowDelimiterBlue guifg=#179fff ctermfg=White]]
+    end,
+  }
 }
