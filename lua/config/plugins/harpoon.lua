825050afe8d413a5fc7d1942d9da428b0483ad0f
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-02-13 21:54:07 +0200

    Fix desc for harpoon

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -14,10 +14,10 @@ return {
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = "Harpoon open picker" })
 
-      vim.keymap.set("n", "<M-a>", function() harpoon:list():select(4) end, { desc = "Harpoon select file 1" })
-      vim.keymap.set("n", "<M-s>", function() harpoon:list():select(3) end, { desc = "Harpoon select file 2" })
-      vim.keymap.set("n", "<M-d>", function() harpoon:list():select(2) end, { desc = "Harpoon select file 3" })
-      vim.keymap.set("n", "<M-f>", function() harpoon:list():select(1) end, { desc = "Harpoon select file 4" })
+      vim.keymap.set("n", "<M-a>", function() harpoon:list():select(4) end, { desc = "Harpoon select file 4" })
+      vim.keymap.set("n", "<M-s>", function() harpoon:list():select(3) end, { desc = "Harpoon select file 3" })
+      vim.keymap.set("n", "<M-d>", function() harpoon:list():select(2) end, { desc = "Harpoon select file 2" })
+      vim.keymap.set("n", "<M-f>", function() harpoon:list():select(1) end, { desc = "Harpoon select file 1" })
 
       vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon previous file" })
       vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Harpoon next file" })
