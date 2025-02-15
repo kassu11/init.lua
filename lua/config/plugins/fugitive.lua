73e714421e6d12771d5a7275f2cc606614004e1c
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-02-15 20:26:29 +0200

    Fugitive keymaps

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -5,7 +5,14 @@ return {
       vim.keymap.set("n", "<leader>gd", ":Gdiffsplit!<CR>", { desc = ":Git diff" })
       vim.keymap.set("n", "<leader>gl", ":0Gclog<CR>", { desc = ":0Gclog" })
       vim.keymap.set("n", "<leader>gL", ":Gclog<CR>", { desc = ":Gclog" })
-      vim.keymap.set("n", "<leader>ge", ":Gedit<CR>", { desc = ":Gedit" })
+      vim.keymap.set("n", "<leader>go", function()
+        local url = vim.api.nvim_exec2("Git remote get-url origin", { output = true })
+        if url.output ~= nil and url.output:sub(1, 8) == "https://" then
+          vim.cmd("!start " .. url.output)
+        else
+          print("No remote origin found")
+        end
+      end, { desc = "Git open remote url" })
     end,
   }
 }
