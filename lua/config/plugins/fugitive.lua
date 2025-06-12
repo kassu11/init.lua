83692c3c9db68240bf09e7a4a138f8cbe61df610
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-06-12 13:32:45 +0300

    Open github.com files from editor

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -15,6 +15,23 @@ return {
         end
       end, { desc = "Git open remote url" })
 
+      vim.keymap.set("n", "<leader>gO", function()
+        local url = vim.api.nvim_exec2("Git remote get-url origin", { output = true })
+        local branch = vim.api.nvim_exec2("Git rev-parse --abbrev-ref HEAD", { output = true })
+        if branch.output == nil then
+          return print("No branch detected")
+        elseif url.output ~= nil and url.output:sub(1, 8) == "https://" then
+          local fixedUrl = url.output
+          if fixedUrl:sub(-4, -1) == ".git" then
+            fixedUrl = fixedUrl:sub(1, -5)
+          end
+          local path = vim.fn.expand('%')
+          vim.cmd("silent !start " .. fixedUrl .. "/blob/" .. branch.output .. "/" .. path)
+        else
+          print("No remote origin found")
+        end
+      end, { desc = "Git open pull request" })
+
       vim.keymap.set("n", "<leader>gp", function()
         local url = vim.api.nvim_exec2("Git remote get-url origin", { output = true })
         local branch = vim.api.nvim_exec2("Git rev-parse --abbrev-ref HEAD", { output = true })
