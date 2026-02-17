b5ede2b9dab997285427f00f93b4e32a452bfde9
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-02-17 14:40:31 +0200

    feat: ✨ Keep selection when adding or subtracking

--- init.lua
+++ init.lua
@@ -133,9 +133,11 @@ vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>//g<Left><Left>")
 vim.keymap.set("x", "<leader>r", "y:%s/\\V<C-r>\"//g<Left><Left>")
 
 vim.keymap.set("n", ">", function() vim.cmd("normal! <<") end, { silent = true, desc = "Indent line" })
-vim.keymap.set("x", ">", function() vim.cmd("normal! <gv") end, { silent = true, desc = "Indent line" })
 vim.keymap.set("n", "<", function() vim.cmd("normal! >>") end, { silent = true, desc = "Indent selection" })
+vim.keymap.set("x", ">", function() vim.cmd("normal! <gv") end, { silent = true, desc = "Indent line" })
 vim.keymap.set("x", "<", function() vim.cmd("normal! >gv") end, { silent = true, desc = "Indent selection" })
+vim.keymap.set("x", "<C-a>", "<C-a>gv", { silent = true, desc = "Add and keep selection" })
+vim.keymap.set("x", "<C-x>", "<C-x>gv", { silent = true, desc = "Subtrack and keep selection" })
 
 
 vim.keymap.set("n", "gp", function() vim.cmd("normal! `[v`]") end, { silent = true, desc = "Select pasted" })
