8929e25fa20eec2565d1b3a293739faa43c2208f
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-04-04 16:15:19 +0300

    feat: ✨ Hide auto completion inside :

--- lua/config/plugins/blink.lua
+++ lua/config/plugins/blink.lua
@@ -6,6 +6,13 @@ return {
     appearance = {
       nerd_font_variant = "mono"
     },
+    cmdline = {
+      completion = {
+        menu = {
+          auto_show = false
+        }
+      },
+    },
     completion = {
       documentation = {
         auto_show = false
