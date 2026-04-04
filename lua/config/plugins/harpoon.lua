a40610abb527ccdd7160ec407d9e7ad27252db84
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-04-04 16:14:09 +0300

    chore: 🧦 Format file

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -16,9 +16,9 @@ return {
 
     for i = 1, #keys, 1 do
       vim.keymap.set("n", "<leader>" .. keys[i], function() harpoon:list():select(i) end,
-      { desc = "Harpoon select file " .. i })
+        { desc = "Harpoon select file " .. i })
       vim.keymap.set("n", "<leader>h" .. keys[i], function() harpoon:list():replace_at(i) end,
-      { desc = "Harpoon replace file " .. i })
+        { desc = "Harpoon replace file " .. i })
     end
   end,
 
