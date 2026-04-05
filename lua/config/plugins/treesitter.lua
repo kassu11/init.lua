b085990c81e9f01c7ff341cacb03d9758c3b96b9
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-04-05 16:52:37 +0300

    feat: ✨ Add treesitter indenting

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -8,5 +8,14 @@ return {
     }
 
     require "nvim-treesitter".install { "rust", "zig", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "jsdoc", "html", "css", "javascript", "typescript", "tsx" }
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
 }
